189e7177913b4d58f5ea81f4965dc2c1457aba42
jianyun2020 <sujianyun2020@163.com>
2022-08-04 11:47:13 +0800

    feat: 基础配置和导航栏

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  title: "Blog",
+  description: "博客测试",
+  themeConfig: {
+    nav: [
+      {
+        text: "首页",
+        link: "/",
+      },
+      {
+        text: "其它",
+        items: [
+          {
+            text: "Github",
+            link: "https://github.com/jianyun2020",
+          },
+          {
+            text: "掘金",
+            link: 'https://juejin.cn/user/976022053862590'
+          }
+        ],
+      },
+    ],
+  },
+};
